191f0afd4ceff34fee5b0da5e1b5d2a1078f50d4
Jose <93064976+Alme08@users.noreply.github.com>
2024-05-11 23:02:04 -0400

    remove useless components

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -1,13 +1,12 @@
-import { View, Text, ScrollView, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import React from 'react';
 import ProductCard from './ProductCard';
-import { productsData } from '../../data/productsData';
 
 const Products = ({ products, loading }) => {
 	if (loading) {
 		return <Text>Loading...</Text>;
 	}
-	if (products.length === 0 && !loading) {
+	if (products?.length === 0 && !loading) {
 		return <Text>No se ha encontrado ningún producto.</Text>;
 	}
 	return (
