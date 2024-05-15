cccb3fb6a50a0e4b71ef8a80bb53d214820064d8
Jose <93064976+Alme08@users.noreply.github.com>
2024-05-14 22:12:04 -0400

    return user data from the server

--- screens/Account/Account.js
+++ screens/Account/Account.js
@@ -1,33 +1,40 @@
 import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import React from 'react';
 import Layout from '../../components/Layout/Layout';
-import { userData } from '../../data/userData';
 import { AntDesign } from '@expo/vector-icons';
+import { useSelector } from 'react-redux';
 
 const Account = ({ navigation }) => {
+	const { user } = useSelector(state => state.user);
+
 	return (
 		<Layout>
 			<View style={styles.container}>
-				<Image source={{ uri: userData.profilePic }} style={styles.image} />
+				<Image
+					source={{
+						uri: user.profilePic
+							? user.profilePic.url
+							: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
+					}}
+					style={styles.image}
+				/>
 				<View style={{ justifyContent: 'center', alignItems: 'center' }}>
-					<Text style={styles.name}>Hi, {userData.name}</Text>
-					<Text>email: {userData.email}</Text>
-					<Text>contact: {userData.contact}</Text>
+					<Text style={styles.name}>Hi, {user.name}</Text>
+					<Text>email: {user.email}</Text>
+					<Text>contact: {user.phone}</Text>
 				</View>
 				<View style={styles.btnContainer}>
 					<Text style={styles.heading}>Account settings</Text>
 					<TouchableOpacity
 						style={styles.btn}
-						onPress={() => navigation.navigate('profile', { id: userData._id })}
+						onPress={() => navigation.navigate('profile', { user: user })}
 					>
 						<AntDesign name='edit' style={styles.btnText} />
 						<Text style={styles.btnText}>Edit Profile</Text>
 					</TouchableOpacity>
 					<TouchableOpacity
 						style={styles.btn}
-						onPress={() =>
-							navigation.navigate('myorders', { id: userData._id })
-						}
+						onPress={() => navigation.navigate('myorders', { id: user._id })}
 					>
 						<AntDesign name='bars' style={styles.btnText} />
 						<Text style={styles.btnText}>My Orders</Text>
@@ -39,15 +46,17 @@ const Account = ({ navigation }) => {
 						<AntDesign name='bells' style={styles.btnText} />
 						<Text style={styles.btnText}>Notifications</Text>
 					</TouchableOpacity>
-					<TouchableOpacity
-						style={styles.btn}
-						onPress={() =>
-							navigation.navigate('adminPanel', { id: userData._id })
-						}
-					>
-						<AntDesign name='laptop' style={styles.btnText} />
-						<Text style={styles.btnText}>Admin Panel</Text>
-					</TouchableOpacity>
+					{user.role === 'admin' && (
+						<TouchableOpacity
+							style={styles.btn}
+							onPress={() =>
+								navigation.navigate('adminPanel', { id: user._id })
+							}
+						>
+							<AntDesign name='laptop' style={styles.btnText} />
+							<Text style={styles.btnText}>Admin Panel</Text>
+						</TouchableOpacity>
+					)}
 				</View>
 			</View>
 		</Layout>
@@ -59,11 +68,14 @@ export default Account;
 const styles = StyleSheet.create({
 	container: {
 		marginVertical: 20,
+		alignItems: 'center',
 	},
 	image: {
 		height: 100,
-		width: '100%',
-		resizeMode: 'contain',
+		width: 100,
+		// resizeMode: 'cover',
+		borderRadius: 100,
+		// resizeMode: 'contain',
 	},
 	name: {
 		marginTop: 10,
@@ -77,6 +89,7 @@ const styles = StyleSheet.create({
 		elevation: 5,
 		borderRadius: 10,
 		paddingBottom: 30,
+		width: '100%',
 	},
 	heading: {
 		fontSize: 20,
