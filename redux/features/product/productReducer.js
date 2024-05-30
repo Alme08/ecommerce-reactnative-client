baea6f51fa8991f1df93cd91d6097b6088716f32
Jose <93064976+Alme08@users.noreply.github.com>
2024-05-29 23:21:02 -0400

    createProduct and updateProductPic cases in productReducer

--- redux/features/product/productReducer.js
+++ redux/features/product/productReducer.js
@@ -9,7 +9,7 @@ export const productReducer = createReducer({}, builder => {
 		state.message = null;
 	});
 
-	// get categories data
+	// get products data
 	builder.addCase('getAllProductDataRequest', (state, action) => {
 		state.loading = true;
 	});
@@ -18,6 +18,30 @@ export const productReducer = createReducer({}, builder => {
 		state.loading = false;
 	});
 	builder.addCase('getAllProductDataFail', (state, action) => {
+		state.error = action.payload;
+	});
+
+	//create product
+	builder.addCase('createProductRequest', (state, action) => {
+		state.loading = true;
+	});
+	builder.addCase('createProductSuccess', (state, action) => {
+		state.loading = false;
+		state.message = action.payload;
+	});
+	builder.addCase('createProductFail', (state, action) => {
+		state.error = action.payload;
+	});
+
+	//update product image
+	builder.addCase('updateProductPicRequest', (state, action) => {
+		state.loading = true;
+	});
+	builder.addCase('updateProductPicSuccess', (state, action) => {
+		state.loading = false;
+		state.message = action.payload;
+	});
+	builder.addCase('updateProductPicFail', (state, action) => {
 		state.error = action.payload;
 	});
 });
