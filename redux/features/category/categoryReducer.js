e8daa7f6681d922ccde725c73fed00b7d13d7569
Jose <93064976+Alme08@users.noreply.github.com>
2024-05-31 23:59:53 -0400

    Add create, update and delete cases

--- redux/features/category/categoryReducer.js
+++ redux/features/category/categoryReducer.js
@@ -20,4 +20,40 @@ export const categoryReducer = createReducer({}, builder => {
 	builder.addCase('getCategoriesDataFail', (state, action) => {
 		state.error = action.payload;
 	});
+
+	// create category
+	builder.addCase('createCategoryRequest', (state, action) => {
+		state.loading = true;
+	});
+	builder.addCase('createCategorySuccess', (state, action) => {
+		state.loading = false;
+		state.message = action.payload;
+	});
+	builder.addCase('createCategoryFail', (state, action) => {
+		state.error = action.payload;
+	});
+
+	// update category
+	builder.addCase('updateCategoryRequest', (state, action) => {
+		state.loading = true;
+	});
+	builder.addCase('updateCategorySuccess', (state, action) => {
+		state.loading = false;
+		state.message = action.payload;
+	});
+	builder.addCase('updateCategoryFail', (state, action) => {
+		state.error = action.payload;
+	});
+
+	// delete category
+	builder.addCase('deleteCategoryRequest', (state, action) => {
+		state.loading = true;
+	});
+	builder.addCase('deleteCategorySuccess', (state, action) => {
+		state.loading = false;
+		state.message = action.payload;
+	});
+	builder.addCase('deleteCategoryFail', (state, action) => {
+		state.error = action.payload;
+	});
 });
