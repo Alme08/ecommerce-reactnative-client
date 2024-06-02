740019b5febba7ddbcd79016449116bc177fe966
Jose <93064976+Alme08@users.noreply.github.com>
2024-06-01 23:55:34 -0400

    create order forms

---
+++ screens/Admin/FormOrders.js
@@ -0,0 +1,140 @@
+import {
+	StyleSheet,
+	Text,
+	TouchableOpacity,
+	View,
+	TextInput,
+} from 'react-native';
+import React, { useState } from 'react';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { Feather } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
+import { useDispatch, useSelector } from 'react-redux';
+import { changeUserStatus } from '../../redux/features/auth/userActions';
+
+const FormOrders = () => {
+	const navigation = useNavigation();
+	const dispatch = useDispatch();
+	const { params } = useRoute();
+	const [userKeyword, setUserKeyword] = useState('');
+	const [orders, setOrders] = useState(params.data);
+	const { users } = useSelector(state => state.user);
+	return (
+		<View>
+			<Text style={styles.title}>Órdenes</Text>
+			<View style={styles.container}>
+				<TextInput
+					placeholder='Buscar cualquier usuario...'
+					placeholderTextColor='#ababab'
+					style={styles.inputBox}
+					value={userKeyword}
+					onChangeText={Text => {
+						setUserKeyword(Text);
+					}}
+				/>
+				<TouchableOpacity style={styles.searchBtn}>
+					<FontAwesome name='search' style={styles.icon} />
+				</TouchableOpacity>
+			</View>
+
+			<View style={styles.container}>
+				<View style={styles.header}>
+					<Text style={{ width: '25%', fontWeight: 'bold' }}>Nombre</Text>
+					<Text style={{ width: '20%', fontWeight: 'bold' }}>Productos</Text>
+					<Text style={{ width: '15%', fontWeight: 'bold' }}>Total</Text>
+					<Text style={{ width: '20%', fontWeight: 'bold' }}>Estado</Text>
+					<Text style={{ width: '10%' }}></Text>
+				</View>
+				{orders.map(order => {
+					let expReg = new RegExp(userKeyword, 'i');
+					if (expReg.test(users.find(user => user._id === order.user).name)) {
+						return (
+							<View key={order._id} style={styles.body}>
+								<Text style={{ width: '25%' }}>
+									{users.find(user => user._id === order.user).name}
+								</Text>
+								<Text style={{ width: '20%' }}>{order.orderItems.length}</Text>
+								<Text style={{ width: '15%' }}>{order.totalAmount}$</Text>
+								<Text style={{ width: '20%' }}>{order.orderStatus}</Text>
+								<View style={styles.actions}>
+									<TouchableOpacity
+										onPress={() => {
+											navigation.navigate('editOrder', { order: order });
+										}}
+									>
+										<Feather name='edit' style={styles.editButton} />
+									</TouchableOpacity>
+								</View>
+							</View>
+						);
+					}
+				})}
+			</View>
+		</View>
+	);
+};
+
+export default FormOrders;
+
+const styles = StyleSheet.create({
+	title: {
+		fontSize: 20,
+		fontWeight: 'bold',
+		textAlign: 'center',
+	},
+	container: {
+		margin: 10,
+		padding: 10,
+		backgroundColor: '#fff',
+		borderRadius: 10,
+	},
+	header: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		paddingBottom: 10,
+	},
+	body: {
+		paddingVertical: 10,
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		borderTopWidth: 1,
+		borderColor: 'lightgray',
+	},
+	actions: {
+		flexDirection: 'row',
+		justifyContent: 'flex-end',
+		gap: 5,
+		width: '10%',
+	},
+	editButton: {
+		padding: 8,
+		backgroundColor: '#1e91cf',
+		fontSize: 15,
+		color: '#fff',
+	},
+	deleteButton: {
+		padding: 8,
+		fontSize: 15,
+		color: '#fff',
+	},
+
+	inputBox: {
+		borderWidth: 0,
+		width: '100%',
+		position: 'relative',
+		left: 15,
+		height: 25,
+		color: '#000000',
+		paddingLeft: 15,
+		fontSize: 16,
+		borderRadius: 10,
+	},
+	searchBtn: {
+		position: 'absolute',
+		right: '98%',
+		top: '55%',
+	},
+});
---
+++ screens/Admin/editOrder.js
@@ -0,0 +1,144 @@
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { format } from 'date-fns';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { changeOrderStatus } from '../../redux/features/orders/orderActions';
+
+const EditOrder = () => {
+	const route = useRoute();
+	const navigation = useNavigation();
+	dispatch = useDispatch();
+	const { order } = route.params;
+	const handleSend = async () => {
+		await dispatch(changeOrderStatus(order._id));
+		navigation.navigate('adminPanel');
+	};
+
+	return (
+		<View>
+			<Text style={styles.title}>Orden</Text>
+			<View style={styles.container}>
+				<View style={styles.orderinfo}>
+					<Text>
+						<Text style={styles.status}>ID Orden :</Text> {order._id}
+					</Text>
+					<Text>{format(order.createdAt, 'dd-MM-yyyy')}</Text>
+				</View>
+				{order.orderItems.map(item => (
+					<View style={styles.item} key={item._id}>
+						<Text>
+							<Text style={styles.status}>Nombre Producto :</Text> {item.name}
+						</Text>
+						<Text>
+							<Text style={styles.status}>Precio :</Text> {item.price}
+						</Text>
+						<Text>
+							<Text style={styles.status}>Cantidad :</Text> {item.quantity}
+						</Text>
+					</View>
+				))}
+				<Text>
+					<Text style={styles.status}>Precio Total :</Text> {order.totalAmount}$
+				</Text>
+				<Text style={styles.status}>Dirección de envío:</Text>
+				<Text style={{ marginLeft: 20 }}>
+					{order.shippingInfo.city}, {order.shippingInfo.address}
+				</Text>
+				<Text style={[styles.status, { fontSize: 15 }]}>Estado: </Text>
+				<Text style={{ marginLeft: 20 }}>{order.orderStatus}</Text>
+			</View>
+
+			<TouchableOpacity style={styles.button} onPress={handleSend}>
+				<Text style={{ color: '#fff', fontSize: 16 }}>Actualizar estado</Text>
+			</TouchableOpacity>
+		</View>
+	);
+};
+
+export default EditOrder;
+
+const styles = StyleSheet.create({
+	title: {
+		fontSize: 20,
+		fontWeight: 'bold',
+		textAlign: 'center',
+	},
+	container: {
+		margin: 10,
+		padding: 10,
+		backgroundColor: '#fff',
+		borderRadius: 10,
+	},
+	textInput: {
+		fontWeight: 'bold',
+		paddingBottom: 5,
+	},
+	input: {
+		padding: 10,
+		borderWidth: 1,
+		borderColor: 'black',
+		height: 40,
+		borderRadius: 10,
+		marginBottom: 20,
+	},
+	picker: {
+		borderWidth: 1,
+		borderColor: 'black',
+		borderRadius: 10,
+		marginBottom: 20,
+	},
+	addImage: {
+		height: 100,
+		width: 80,
+		borderRadius: 10,
+		backgroundColor: 'gray',
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	image: {
+		height: 100,
+		width: 80,
+		borderRadius: 10,
+	},
+	deleteImage: {
+		position: 'absolute',
+		zIndex: 1,
+		backgroundColor: '#fff',
+		borderRadius: 50,
+		margin: 5,
+		left: 50,
+	},
+	button: {
+		marginHorizontal: 'auto',
+		alignItems: 'center',
+		padding: 10,
+		backgroundColor: 'green',
+		width: 180,
+		borderRadius: 10,
+	},
+	container: {
+		backgroundColor: '#fff',
+		margin: 10,
+		padding: 10,
+		borderRadius: 10,
+	},
+	orderinfo: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		borderBottomWidth: 1,
+		borderColor: 'lightgray',
+		paddingBottom: 5,
+	},
+	item: {
+		borderBottomWidth: 1,
+		borderColor: 'lightgray',
+		padding: 5,
+	},
+	status: {
+		// borderTopWidth: 1,
+		fontWeight: 'bold',
+		// borderColor: 'lightgray',
+		padding: 5,
+	},
+});
