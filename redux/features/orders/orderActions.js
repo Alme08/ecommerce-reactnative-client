e52ce01f1dcdec3fc7639dc0975647e25c3a31ee
Jose <93064976+Alme08@users.noreply.github.com>
2024-06-01 23:54:28 -0400

    Add updateStatus and getOrders to orderActions

--- redux/features/orders/orderActions.js
+++ redux/features/orders/orderActions.js
@@ -15,7 +15,45 @@ export const getAllOrdersData = () => async dispatch => {
 		});
 	} catch (error) {
 		dispatch({
-			type: 'getAllProductDataFail',
+			type: 'getAllOrdersDataFail',
+			payload: error.response.data.message,
+		});
+	}
+};
+
+// GET ALL ORDERS
+export const getAllOrders = () => async dispatch => {
+	try {
+		dispatch({
+			type: 'getAllOrdersDataAdminRequest',
+		});
+		const { data } = await axios.get(`${server}/order/admin/get-all-orders`);
+		dispatch({
+			type: 'getAllOrdersDataAdminSuccess',
+			payload: data?.orders,
+		});
+	} catch (error) {
+		dispatch({
+			type: 'getAllOrdersDataAdminFail',
+			payload: error.response.data.message,
+		});
+	}
+};
+
+// CHANGE ORDER STATUS
+export const changeOrderStatus = id => async dispatch => {
+	try {
+		dispatch({
+			type: 'changeOrderStatusRequest',
+		});
+		const { data } = await axios.put(`${server}/order/admin/order/${id}`);
+		dispatch({
+			type: 'changeOrderStatusSuccess',
+			payload: data?.order,
+		});
+	} catch (error) {
+		dispatch({
+			type: 'changeOrderStatusFail',
 			payload: error.response.data.message,
 		});
 	}
